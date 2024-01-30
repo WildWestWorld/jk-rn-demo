aec94e438405a160d18abfe2c79d9c00e23a2111
ljk <380686356@qq.com>
2024-01-30 20:44:06 +0800

    完成FlatListDemo

--- src/components/FlatListDemo.js
+++ src/components/FlatListDemo.js
@@ -1,10 +1,36 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 export default function FlatListDemo() {
+
+    const flatListRef = useRef(null)
+
+    useEffect(() => {
+        setTimeout(() => {
+            // flatListRef.current.scrollToIndex({
+            //     index: 10,
+            //     viewPosition:0.5,
+            //     animated:true
+            // })
+
+            // flatListRef.current.scrollToItem({
+            //     item: 10,
+            //     viewPosition: 0.5,
+            //     animted: true
+            // })
+
+            // flatListRef.current.scrollToOffset({
+            //     offset: 200
+            // })
+
+            flatListRef.current.scrollToEnd({ animated: true })
+
+        }, 2000)
+
+    }, [])
 
     const renderItem = ({ item, index }) => {
         return (
@@ -18,9 +44,24 @@ export default function FlatListDemo() {
         )
     }
 
-    const ListHeader = (<View style={styles.header}><Text>123</Text></View>)
+    const ListHeader = (
+        <View style={styles.header}
+        ><Text>列表头部</Text>
+        </View>
+    )
 
-    const ListFooter = (<View style={styles.footer}></View>)
+    const ListFooter = (
+        <View style={[styles.header, styles.footer]}>
+            <Text>列表尾部</Text>
+        </View>
+    )
+
+    const ListEmpty = (
+        <View style={styles.listEmpty}>
+            <Text style={styles.extraTxt}>列表为空</Text>
+        </View>
+    )
+
 
     return (
         // <FlatList style={styles.flatlist} data={data} renderItem={renderItem} keyExtractor={(item, index) => `item-${index}`} contentContainerStyle={styles.containerStyle} showsVerticalScrollIndicator={false} 
@@ -31,16 +72,27 @@ export default function FlatListDemo() {
         //  keyboardShouldPersistTaps='handled'
         // ></FlatList>
 
-        <FlatList style={styles.flatlist2} data={data} renderItem={renderItem2} keyExtractor={(item, index) => `item-${index}`} contentContainerStyle={styles.containerStyle} showsVerticalScrollIndicator={false}
+        <FlatList ref={flatListRef} style={styles.flatlist2} data={data} renderItem={renderItem} keyExtractor={(item, index) => `item-${index}`} contentContainerStyle={styles.containerStyle} showsVerticalScrollIndicator={false}
             onScroll={(event) => {
-                console.log(event.nativeEvent.contentOffset.x);
+                console.log(event.nativeEvent.contentOffset.y);
             }}
             keyboardDismissMode='on-drag'
             keyboardShouldPersistTaps='handled'
-            horizontal={true}
+            horizontal={false}
             showsHorizontalScrollIndicator={false}
             ListHeaderComponent={ListHeader}
             ListFooterComponent={ListFooter}
+            ListEmptyComponent={ListEmpty}
+            ItemSeparatorComponent={
+                <View style={styles.separator}>
+                </View>}
+            initialNumToRender={15}
+            inverted={false}
+            numColumns={1}
+            onViewableItemsChanged={(info) => {
+                const { viewableItems } = info;
+                console.log(viewableItems);
+            }}
         ></FlatList>
     )
 }
@@ -74,5 +126,36 @@ const styles = StyleSheet.create({
         paddingHorizontal: 16,
         paddingTop: 20,
         backgroundColor: '#F5F5F5'
+    },
+    header: {
+        width: '100%',
+        height: 48,
+        backgroundColor: '#00ff0030',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    footer: {
+        width: '100%',
+        height: 48,
+        backgroundColor: '#00ff0030',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    listEmpty: {
+        width: '100%',
+        height: 400,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    extraTxt: {
+
+        fontSize: 20,
+        color: '#666666',
+        textAlignVertical: 'center'
+    },
+    separator: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#D0D0D0'
     }
 })
